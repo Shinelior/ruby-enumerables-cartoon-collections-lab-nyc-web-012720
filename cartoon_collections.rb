6ea6b6b58f4c0154f3d8ccdfb184c3e01520f589
lior Shine <shinelior@gmail.com>
2020-01-21 23:38:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -42,8 +42,10 @@ def find_the_cheese(aar)
   cheese_types = ["cheddar", "gouda", "camembert"]
   aar.include?(cheese_types)
   i = 0 
-  while i < 
-  result  = aar.find do |x| x==cheese_types[i] 
+  while i < cheese_types.length
+  result  = aar.find do |x| x == cheese_types[i]
+  i += 1
+end
   binding.pry
 end
 end
