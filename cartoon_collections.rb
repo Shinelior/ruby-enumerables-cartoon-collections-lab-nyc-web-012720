7eff35fe5f483045dda156a627481ce171702027
lior Shine <shinelior@gmail.com>
2020-01-21 23:44:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -43,7 +43,7 @@ def find_the_cheese(aar)
   aar.include?(cheese_types)
  # i = 0 
   #while i < cheese_types.length
-  result  = aar.find do |x| x == cheese_types
+  result  = aar.find do |x|  x == cheese_types
 # binding.pry
 end
  
